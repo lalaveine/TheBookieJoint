0576a877421695546d5276b8db1d04538f9de95e
Evgeniy Matveev <mfb.eugene@gmail.com>
2018-06-07 19:07:34 +0300

    almost finished cart

--- Startup.cs
+++ Startup.cs
@@ -25,6 +25,8 @@ namespace TheBookieJoint
                 options.UseNpgsql(Configuration["Data:TheBookieJoint:ConnectionString"]));
             services.AddTransient<IProductRepository, EFProductRepository>();
             services.AddMvc();
+            services.AddMemoryCache();
+            services.AddSession();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,6 +40,7 @@ namespace TheBookieJoint
             app.UseDeveloperExceptionPage();
             app.UseStatusCodePages();
             app.UseStaticFiles();
+            app.UseSession();
             app.UseMvc(routes => {
                 routes.MapRoute(
                     name: null,
---
+++ Views/ViewModels/CartIndexViewModel.cs
@@ -0,0 +1,8 @@
+using TheBookieJoint.Models;
+
+namespace TheBookieJoint.Models.ViewModels {
+    public class CartIndexViewModel {
+        public Cart Cart { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
