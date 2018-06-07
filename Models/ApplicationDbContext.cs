2fe515c165a4dbbf1e2627a0346fd847960eaeb9
Evgeniy Matveev <mfb.eugene@gmail.com>
2018-06-07 22:41:27 +0300

    customer is complete

--- Models/ApplicationDbContext.cs
+++ Models/ApplicationDbContext.cs
@@ -6,6 +6,8 @@ namespace TheBookieJoint.Models {
     public class ApplicationDbContext : DbContext {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
         public DbSet<Product> Products { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
     }
 }
 
--- Startup.cs
+++ Startup.cs
@@ -31,6 +31,7 @@ namespace TheBookieJoint
             services.AddTransient<IProductRepository, EFProductRepository>();
             services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddTransient<IOrderRepository, EFOrderRepository>();
             services.AddMvc();
             services.AddMemoryCache();
             services.AddSession();
